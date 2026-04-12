218ee3a56e4efbc1b9a00b91e919e1997a5af051
Ronnie <royalronnie05@gmail.com>
2026-04-12 14:23:16 +0530

    added calculator

---
+++ Caluculator/calculator.js
@@ -0,0 +1,6 @@
+let display = document.querySelector("#result");
+
+// document.querySelector("#btn-1").addEventListener("click", (e) => {
+//   alert("Helllo");
+//   console.log(e);
+// });
---
+++ Digital clock/digital.js
@@ -0,0 +1,14 @@
+let hours = document.getElementById("hr");
+let minutes = document.getElementById("min");
+let seconds = document.getElementById("sec");
+
+setInterval(() => {
+  const currentTime = new Date();
+
+  hours.innerHTML =
+    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+  minutes.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  seconds.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+});
