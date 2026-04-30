adfe6acdf5ca271273de718be0afe2420557500f
Ronnie <royalronnie05@gmail.com>
2026-04-30 13:21:24 +0530

    added rgb color

--- Colour changer/colour.js
+++ Colour changer/colour.js
@@ -6,12 +6,18 @@ document.querySelector("#bttn").addEventListener("click", () => {
 });
 
 function getRandomColor() {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
+  // let letters = "0123456789ABCDEF";
+  // let color = "#";
 
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
+  // for (let i = 0; i < 6; i++) {
+  //   color += letters[Math.floor(Math.random() * 16)];
+  // }
+
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  let color = `rgb(${r},${g},${b})`;
 
   colorsName.innerHTML = color;
   return color;
