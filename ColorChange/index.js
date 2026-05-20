fe23223944b181f0f931dd4d09daa8f1346c9623
Ronnie <royalronnie05@gmail.com>
2026-05-20 08:42:58 +0530

    added new feature

--- ColorChange/index.js
+++ ColorChange/index.js
@@ -1,4 +1,8 @@
 let body = document.querySelector("body");
+let hour = document.querySelector("#hour");
+let minutes = document.querySelector("#minutes");
+let seconds = document.querySelector("#seconds");
+
 function changeBackgroundcolor() {
   let initial = "#";
   let colorcode = "0123456789ABCDEF";
@@ -12,3 +16,16 @@ function changeBackgroundcolor() {
 setInterval(() => {
   changeBackgroundcolor();
 }, 2000);
+
+function updateClock() {
+  let getTime = new Date();
+  let hours = (getTime.getHours() < 10 ? "0" : "") + getTime.getHours();
+  let min = (getTime.getMinutes() < 10 ? "0" : "") + getTime.getMinutes();
+  let sec = (getTime.getSeconds() < 10 ? "0" : "") + getTime.getSeconds();
+
+  hour.textContent = hours;
+  minutes.textContent = min;
+  seconds.textContent = sec;
+}
+updateClock();
+setInterval(updateClock, 1000);
