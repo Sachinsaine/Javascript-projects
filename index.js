7ef1d9fdfc8b1e78ad816a5760a3c6ff7c911bca
Ronnie <royalronnie05@gmail.com>
2026-06-17 08:41:03 +0530

    js practice

--- index.js
+++ index.js
@@ -29,3 +29,84 @@ console.log(thirdLargest([5, 5, 3, 2]));
 console.log(thirdLargest([10, 5, 20, 8, 15]));
 console.log(thirdLargest([1, 2]));
 console.log(thirdLargest([7, 7, 7]));
+
+function firstCharAppearingTwice(str) {
+  let freq = {};
+  for (let num of str) {
+    freq[num] = (freq[num] || 0) + 1;
+  }
+  for (let char of str) {
+    if (freq[char] === 2) {
+      return char;
+    }
+  }
+  return null;
+}
+
+console.log(firstCharAppearingTwice("abccde"));
+console.log(firstCharAppearingTwice("aabbc"));
+console.log(firstCharAppearingTwice("abcdef"));
+console.log(firstCharAppearingTwice("aabccdd"));
+
+function groupNumbers(arr) {
+  let group = {
+    positive: [],
+    negative: [],
+    zero: [],
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      group.positive.push(arr[i]);
+    } else if (arr[i] < 0) {
+      group.negative.push(arr[i]);
+    } else {
+      group.zero.push(arr[i]);
+    }
+  }
+  return group;
+}
+
+console.log(groupNumbers([1, -2, 0, 5, -3]));
+console.log(groupNumbers([0, 0, 1]));
+console.log(groupNumbers([-1, -2, -3]));
+
+function mostFrequentWord(arr) {
+  let freq = {};
+  let max = 0;
+  let result = "";
+  for (let char of arr) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+  for (let key of arr) {
+    if (freq[key] > max) {
+      max = freq[key];
+      result = key;
+    }
+  }
+  return result === "" ? null : result;
+}
+
+console.log(mostFrequentWord(["apple", "banana", "apple", "orange"]));
+console.log(mostFrequentWord(["cat", "dog", "cat", "dog"]));
+console.log(mostFrequentWord(["hello"]));
+console.log(mostFrequentWord([]));
+
+function lastNonRepeatingChar(str) {
+  let freq = {};
+  let result = [];
+  for (let char of str) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+  for (let num of str) {
+    if (freq[num] === 1) {
+      result.push(num);
+    }
+  }
+  return result.length === 0 ? null : result.pop();
+}
+
+console.log(lastNonRepeatingChar("abacbd"));
+console.log(lastNonRepeatingChar("aabb"));
+console.log(lastNonRepeatingChar("abc"));
+console.log(lastNonRepeatingChar("aabc"));
