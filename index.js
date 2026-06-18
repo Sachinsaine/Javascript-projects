2a5a3e5f6d2cf3f7fdeb6e6278e1be4978d48d67
Ronnie <royalronnie05@gmail.com>
2026-06-18 06:48:05 +0530

    test

--- index.js
+++ index.js
@@ -1,112 +1,63 @@
-function isIncreasing(arr) {
-  if (arr.length === 0) return false;
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] >= arr[i + 1]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-console.log(isIncreasing([1, 2, 3, 4]));
-console.log(isIncreasing([1, 3, 2]));
-console.log(isIncreasing([5]));
-console.log(isIncreasing([2, 2, 3]));
-
-function thirdLargest(arr) {
-  let result = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (!result.includes(arr[i])) {
-      result.push(arr[i]);
-    }
-  }
-  let temp = result.sort((a, b) => a - b).reverse();
-  return temp[2] === undefined ? null : temp[2];
-}
-
-console.log(thirdLargest([5, 5, 3, 2]));
-console.log(thirdLargest([10, 5, 20, 8, 15]));
-console.log(thirdLargest([1, 2]));
-console.log(thirdLargest([7, 7, 7]));
-
-function firstCharAppearingTwice(str) {
+function firstUniqueChar(str) {
+  const unique = "";
   let freq = {};
-  for (let num of str) {
-    freq[num] = (freq[num] || 0) + 1;
-  }
   for (let char of str) {
-    if (freq[char] === 2) {
-      return char;
+    freq[char] = (freq[char] || 0) + 1;
+  }
+  for (let key of str) {
+    if (freq[key] === 1) {
+      return key;
     }
   }
   return null;
 }
+console.log(firstUniqueChar("javascript"));
+console.log(firstUniqueChar("aabbcc"));
+console.log(firstUniqueChar("swiss"));
 
-console.log(firstCharAppearingTwice("abccde"));
-console.log(firstCharAppearingTwice("aabbc"));
-console.log(firstCharAppearingTwice("abcdef"));
-console.log(firstCharAppearingTwice("aabccdd"));
-
-function groupNumbers(arr) {
-  let group = {
-    positive: [],
-    negative: [],
-    zero: [],
-  };
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      group.positive.push(arr[i]);
-    } else if (arr[i] < 0) {
-      group.negative.push(arr[i]);
-    } else {
-      group.zero.push(arr[i]);
-    }
-  }
-  return group;
-}
-
-console.log(groupNumbers([1, -2, 0, 5, -3]));
-console.log(groupNumbers([0, 0, 1]));
-console.log(groupNumbers([-1, -2, -3]));
-
-function mostFrequentWord(arr) {
+function maxChar(str) {
   let freq = {};
   let max = 0;
   let result = "";
-  for (let char of arr) {
+  for (let char of str) {
     freq[char] = (freq[char] || 0) + 1;
   }
-  for (let key of arr) {
+  for (let key of str) {
     if (freq[key] > max) {
       max = freq[key];
       result = key;
     }
   }
-  return result === "" ? null : result;
+  return result;
 }
+console.log(maxChar("javascript"));
+console.log(maxChar("aaabbbbcc"));
+console.log(maxChar("abcd"));
+console.log(maxChar("aabb"));
 
-console.log(mostFrequentWord(["apple", "banana", "apple", "orange"]));
-console.log(mostFrequentWord(["cat", "dog", "cat", "dog"]));
-console.log(mostFrequentWord(["hello"]));
-console.log(mostFrequentWord([]));
+function isAnagram(str1, str2) {
+  let temp1 = str1.toLowerCase().replace(/\s+/g, "").split("").sort().join("");
+  let temp2 = str2.toLowerCase().replace(/\s+/g, "").split("").sort().join("");
 
-function lastNonRepeatingChar(str) {
-  let freq = {};
-  let result = [];
-  for (let char of str) {
-    freq[char] = (freq[char] || 0) + 1;
-  }
-  for (let num of str) {
-    if (freq[num] === 1) {
-      result.push(num);
+  return temp1 === temp2 ? true : false;
+}
+console.log(isAnagram("listen", "silent"));
+console.log(isAnagram("hello", "world"));
+console.log(isAnagram("triangle", "integral"));
+console.log(isAnagram("Dormitory", "Dirty Room"));
+
+function firstDuplicate(arr) {
+  let freq = new Set();
+
+  for (let key of arr) {
+    if (freq.has(key)) {
+      return key;
     }
+    freq.add(key);
   }
-  return result.length === 0 ? null : result.pop();
+  return null;
 }
-
-console.log(lastNonRepeatingChar("abacbd"));
-console.log(lastNonRepeatingChar("aabb"));
-console.log(lastNonRepeatingChar("abc"));
-console.log(lastNonRepeatingChar("aabc"));
+console.log(firstDuplicate([2, 5, 1, 2, 3, 5, 1]));
+console.log(firstDuplicate([1, 2, 3, 4]));
+console.log(firstDuplicate([5, 5, 5, 5]));
+console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
