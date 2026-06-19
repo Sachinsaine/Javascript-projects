45ed9119057db6c3258e09d6da6b24d58b88f68f
Ronnie <royalronnie05@gmail.com>
2026-06-19 17:22:16 +0530

    leetcode

--- index.js
+++ index.js
@@ -1,63 +1,64 @@
-function firstUniqueChar(str) {
-  const unique = "";
-  let freq = {};
-  for (let char of str) {
-    freq[char] = (freq[char] || 0) + 1;
-  }
-  for (let key of str) {
-    if (freq[key] === 1) {
-      return key;
+function twoSum(nums, target) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        result.push(i, j);
+      }
     }
   }
-  return null;
+  return result.length === 0 ? null : result;
 }
-console.log(firstUniqueChar("javascript"));
-console.log(firstUniqueChar("aabbcc"));
-console.log(firstUniqueChar("swiss"));
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
 
-function maxChar(str) {
+function containsDuplicate(arr) {
+  if (arr.length === 0) return false;
   let freq = {};
-  let max = 0;
-  let result = "";
-  for (let char of str) {
-    freq[char] = (freq[char] || 0) + 1;
+
+  for (let num of arr) {
+    freq[num] = (freq[num] || 0) + 1;
   }
-  for (let key of str) {
-    if (freq[key] > max) {
-      max = freq[key];
-      result = key;
-    }
-  }
-  return result;
-}
-console.log(maxChar("javascript"));
-console.log(maxChar("aaabbbbcc"));
-console.log(maxChar("abcd"));
-console.log(maxChar("aabb"));
-
-function isAnagram(str1, str2) {
-  let temp1 = str1.toLowerCase().replace(/\s+/g, "").split("").sort().join("");
-  let temp2 = str2.toLowerCase().replace(/\s+/g, "").split("").sort().join("");
-
-  return temp1 === temp2 ? true : false;
-}
-console.log(isAnagram("listen", "silent"));
-console.log(isAnagram("hello", "world"));
-console.log(isAnagram("triangle", "integral"));
-console.log(isAnagram("Dormitory", "Dirty Room"));
-
-function firstDuplicate(arr) {
-  let freq = new Set();
-
   for (let key of arr) {
-    if (freq.has(key)) {
-      return key;
+    if (freq[key] > 1) {
+      return true;
     }
-    freq.add(key);
   }
-  return null;
+  return false;
 }
-console.log(firstDuplicate([2, 5, 1, 2, 3, 5, 1]));
-console.log(firstDuplicate([1, 2, 3, 4]));
-console.log(firstDuplicate([5, 5, 5, 5]));
-console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
+console.log(containsDuplicate([1, 2, 3, 1, 0, 0]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+
+function findAnagram(str1, str2) {
+  let freq = {};
+  for (let char of str1) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+  for (let char of str2) {
+    if (!freq[char]) {
+      return false;
+    }
+    freq[char]--;
+  }
+  return true;
+}
+console.log(findAnagram("anagram", "nagaram"));
+console.log(findAnagram("rat", "car"));
+
+function missingNumber(arr) {
+  let n = arr.length;
+  let actualSum = (n * (n + 1)) / 2;
+  let currentSum = 0;
+  for (let num of arr) {
+    currentSum += num;
+  }
+
+  return actualSum - currentSum;
+}
+console.log(missingNumber([3, 0, 1]));
+console.log(missingNumber([0, 1]));
+console.log(missingNumber([2, 4, 6, 0, 1, 3, 5, 7, 9]));
+
+// Leetcode 5 problems are done.
