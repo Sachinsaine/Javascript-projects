862f3d9448d0ce284626bdf163be3d6b46f37a13
Ronnie <royalronnie05@gmail.com>
2026-06-20 12:57:13 +0530

    leetcode day-2

--- index.js
+++ index.js
@@ -61,4 +61,84 @@ console.log(missingNumber([3, 0, 1]));
 console.log(missingNumber([0, 1]));
 console.log(missingNumber([2, 4, 6, 0, 1, 3, 5, 7, 9]));
 
-// Leetcode 5 problems are done.
+function singleNumber(arr) {
+  if (arr.length === 0) return null;
+  let freq = {};
+  for (let num of arr) {
+    freq[num] = (freq[num] || 0) + 1;
+  }
+
+  for (let key of arr) {
+    if (freq[key] === 1) {
+      return key;
+    }
+  }
+  return null;
+}
+console.log(singleNumber([2, 2, 1]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
+console.log(singleNumber([1]));
+
+function isPalindrome(str) {
+  let temp = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  let revese = temp.split("").reverse("").join("");
+  return temp === revese;
+}
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+console.log(isPalindrome("Race a Car"));
+
+function longestCommonPrefix(arr) {
+  let prefix = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    while (!arr[i].startsWith(prefix)) {
+      prefix = prefix.slice(0, -1);
+
+      if (prefix === "") {
+        return "";
+      }
+    }
+  }
+  return prefix;
+}
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+
+function moveZeroes(nums) {
+  if (nums.length === 0) return null;
+  let zeros = [];
+  let nonZeros = [];
+  for (let num of nums) {
+    if (num === 0) {
+      zeros.push(num);
+    } else {
+      nonZeros.push(num);
+    }
+  }
+  return [...nonZeros, ...zeros];
+}
+console.log(moveZeroes([0, 1, 0, 3, 12]));
+
+function removeDuplicates(arr) {
+  if (arr.length == 0) return null;
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[j]) {
+      j++;
+      arr[j] = arr[i];
+    }
+  }
+  return arr.slice(0, j + 1);
+}
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+console.log(removeDuplicates([1, 1, 2]));
+
+function sortedSquares(arr) {
+  let initial = 1;
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    initial = arr[i] * arr[i];
+    // console.log(initial);
+    result.push(initial);
+    console.log(initial);
+  }
+}
+console.log(sortedSquares([3, 10]));
