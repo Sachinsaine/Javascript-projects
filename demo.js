e8bde9528d15e1428ca684cf067c7f6ffd15ff54
Ronnie <royalronnie05@gmail.com>
2026-08-06 15:00:00 +0530

    js problem

--- demo.js
+++ demo.js
@@ -1,37 +1,7 @@
-function greet(name) {
-  setTimeout(() => {
-    console.log(`Hello, ${name}`);
-  }, 2000);
-}
-let result = greet("Ronnie");
-console.log(result);
+const express = require("express");
+const app = express();
+const PORT = 4000;
 
-function demo(a, b, c) {
-  return a + b + c;
-}
-console.log(demo(1, 2, 3));
-
-function add(x) {
-  return x + 5;
-}
-
-function multiple(y) {
-  return y * 2;
-}
-
-let temp = multiple(add(10));
-console.log(temp);
-
-function* generateId() {
-  let id = 1;
-
-  while (true) {
-    yield id++;
-  }
-}
-
-const ids = generateId();
-
-console.log(ids.next().value); // 1
-console.log(ids.next().value); // 2
-console.log(ids.next().value); // 3
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
