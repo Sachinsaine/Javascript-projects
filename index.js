ddd54efeaf47441459a58c0ddf7f1fa946c73889
Ronnie <royalronnie05@gmail.com>
2026-08-15 11:49:03 +0530

    two pointers

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ function isValidPalindrome(str) {
 console.log(isValidPalindrome("A man, a plan, a canal: Panama"));
 console.log(isValidPalindrome("race a car"));
 console.log(isValidPalindrome(" "));
+
+function maxWater(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  let max = 0;
+
+  while (left < right) {
+    let height = Math.min(arr[left], arr[right]);
+    let width = right - left;
+    let area = height * width;
+
+    if (arr[left] < arr[right]) {
+      left++;
+    } else {
+      right--;
+    }
+
+    max = Math.max(max, area);
+  }
+  return max;
+}
+
+console.log(maxWater([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+console.log(maxWater([1, 1]));
