64f6e72362c7e15edf4d8f915c5ad29b63cbdf00
Ronnie <royalronnie05@gmail.com>
2026-09-09 11:24:05 +0530

    js

--- index.js
+++ index.js
@@ -1,61 +1,85 @@
-function checkArmStrongNum(num) {
-  let n = num.toString();
-  let digits = n.length;
-  let sum = 0;
-
-  for (let num of n) {
-    sum += Math.pow(parseInt(num), digits);
-  }
-  return sum === num;
-}
-console.log(checkArmStrongNum(153));
-console.log(checkArmStrongNum(9474));
-
-function isPerfectNum(num) {
-  let sum = 0;
-  for (let i = 1; i < num; i++) {
-    if (num % i === 0) {
-      sum += i;
+function findMaxNum(arr) {
+  if (arr.length === 0) return null;
+  let max = arr[0];
+  for (let num of arr) {
+    if (num > max) {
+      max = num;
     }
   }
-  return sum === num;
+  return max;
 }
-console.log(isPerfectNum(6));
+console.log(findMaxNum([12, 45, 7, 89, 34, 56]));
+console.log(findMaxNum([-10, -5, -20, -3]));
 
-function findSum(num) {
-  let digits = num.toString();
-  let sum = 0;
-  for (let i = 0; i < digits.length; i++) {
-    sum += parseInt(digits[i]);
+function findSmallestNum(arr) {
+  if (arr.length === 0) return null;
+
+  let min = arr[0];
+  for (let num of arr) {
+    if (num < min) {
+      min = num;
+    }
   }
-  return sum;
+  return min;
 }
-console.log(findSum(1234));
-console.log(findSum(567));
+console.log(findSmallestNum([23, 5, 89, 12, 3, 45]));
 
-function reverseNum(num) {
-  let temp = num.toString();
+function countEvenNum(arr) {
+  if (arr.length === 0) return null;
+  let count = 0;
+
+  for (let num of arr) {
+    if (num % 2 === 0) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countEvenNum([12, 7, 4, 9, 18, 21, 6]));
+
+function reverseStr(str) {
+  if (str.length === 0) return null;
   let result = "";
-  for (let i = temp.length - 1; i >= 0; i--) {
-    result += temp[i];
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
   }
-  return Number(result);
+  return result;
 }
-console.log(reverseNum(1234));
 
-function isPrimeNum(num) {
-  if (num <= 1) return false;
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false;
+console.log(reverseStr("hello"));
+console.log(reverseStr("A"));
+console.log(reverseStr(""));
+console.log(reverseStr("12345"));
+
+function countVowels(str) {
+  if (str.length === 0) return -1;
+  str = str.toLowerCase();
+  let count = 0;
+  const vowels = "aeiou";
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
     }
   }
-  return true;
+  return count;
 }
-console.log(isPrimeNum(113));
-console.log(isPrimeNum(123));
+console.log(countVowels("javascript"));
 
-function LCM(num1, num2) {
-  
+function firstDuplicateNum(arr) {
+  if (arr.length === 0) return null;
+
+  let freq = {};
+  for (let num of arr) {
+    freq[num] = (freq[num] || 0) + 1;
+  }
+
+  for (let num of arr) {
+    if (freq[num] > 1) {
+      return num;
+    }
+    freq[num] = true;
+  }
+  return null;
 }
-console.log(LCM(4,6));
+console.log(firstDuplicateNum([2, 5, 1, 2, 3, 5]));
