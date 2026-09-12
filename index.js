67d80a314111a4c375d657ecde460f249e79ce61
Ronnie <royalronnie05@gmail.com>
2026-09-12 11:28:35 +0530

    js

--- index.js
+++ index.js
@@ -377,3 +377,24 @@ function findFirstUnique(arr) {
   return null;
 }
 console.log(findFirstUnique([4, 5, 1, 2, 0, 4, 1, 2]));
+
+function longestConsecutive(arr) {
+  let set = new Set(arr);
+  let longest = 0;
+
+  for (let num of arr) {
+    if (!set.has(num - 1)) {
+      let current = num;
+      let count = 1;
+
+      while (set.has(current + 1)) {
+        current++;
+        count++;
+      }
+
+      longest = Math.max(longest, count);
+    }
+  }
+  return longest;
+}
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]));
